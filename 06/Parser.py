a34e1cb4968a76cb791d704f83e21a1769dc811f
dserver <jonathanhenk@gmail.com>
2016-02-24 00:05:39 -0500

    Uploaded the incorrect parser file. Previous commit message was correct, though

--- 06/Parser.py
+++ 06/Parser.py
@@ -5,7 +5,7 @@ import string, os
 
 
 # copies the original assembly file into another file where
-# all comments and newlines are removed for easy translation in the 
+# all comments and newlines are removed for easy translation in the
 # parser functions
 def copy_and_clean(asm_file):
     cp_file = open(os.getenv('TEMP') + '\\temp_assembly_file.txt', 'w+')
@@ -18,26 +18,22 @@ def copy_and_clean(asm_file):
         # remove comments and whitespace
         line = clean_line(line)
 
-        # ... after all processing, write line if its still relevant
-        # ie. it has some code in it
+        # after all processing, write line to final assembly file if its still relevant
+        # ie. the line will translate to a binary instruction
         if (line != '' and line != '\n'):
             cp_file.write(line)
             cp_file.flush() # garbage gets written sometimes. Not sure why
 
     orig_file.close()
+    cp_file.close()
+    return
 
-    return cp_file
-
+# removes all comments and whitespace
 def clean_line(line):
     # remove comments
     comment = line.find("//")
     if (comment > -1):
         line = line[0:comment]
-
-    # # remove newline
-    # newline = line.find("\n")
-    # if (newline > -1):
-    #     line = line[0:newline]
 
     # remove white space
     if (line.find(" ") > -1):
@@ -46,33 +42,25 @@ def clean_line(line):
     return line
 
 class Parser:
+    commands = [] # each line in the input assembly file is stored here
     command = "" # current command
+    command_i = 0 # which 'line' in the file we're on
 
-
-    def __init__(filename):
-        this.asm_file = open(filename, 'r')
+    def __init__(self, filename):
+        copy_and_clean(filename)
+        asm_file = open(os.getenv('TEMP') + '\\temp_assembly_file.txt', 'r')
+        for line in asm_file:
+            self.commands.append(line)
 
     # returns if there are any commands left to parse
-    def hasMoreCommands():
-        spot = this.asm_file.tell() # save place holder at current line
-        com = this.asm_file.readline()
-
-
-        if (com == ''):
+    def hasMoreCommands(self):
+        if (self.command_i == len(self.commands)):
             return False
-        else:
-            this.asm_file.seek(spot) # jump back to place before readline()
-            return True
+        return True
 
     # read next command. Skips whitespace and comments
-    def advance():
-        com = this.asm_file.readline()
-
-        if (str.find(com, '=') >= 0): # X=Y type command
-            a=1
-        else if (str.find(com, ';') >= 0): # D;JMP command
-            a=1
-        else # ....
+    def advance(self):
+        self.command_i += 1
 
     def symbol():
 
@@ -81,31 +69,3 @@ class Parser:
     def comp():
 
     def jmp():
-
-
-
-Parser:
-    Constructor(file) - open file and get ready to parse!
-
-    hasMoreCommands - true of stream isn't empty
-    advance - read next command from input and sets it to current command
-
-    commandType - returns type of current command
-                    A_COMMAND, C_COMMAND, L_COMMAND [ for (xxx) commands]
-
-    symbol - returns symbol or decimal of current command only if
-             current command is of type A_COMMAND or L_COMMAND
-             returns the @xxx (xxx) xxx part
-
-    dest - returns the dest mnemonic in the current C_COMMAND
-           8 possiblities
-           
-    comp - returns the comp mnemonic in the current C_COMMAND
-            some number of possibilities
-    jmp - returns the jmp mnemonic in the current C_COMMAND
-
-
-Code:
-    dest - returns binary form of dest
-    comp - same
-    jump - same
